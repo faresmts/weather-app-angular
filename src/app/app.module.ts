3530f0a3a8bdb838fcebb8a0b4cd960939b56f22
fares <fares@stargrid.pro>
2024-01-19 20:56:05 -0300

    chore: project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  // We import modules to application here
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    FontAwesomeModule
   ],
+  // DI
   providers: [],
   bootstrap: [AppComponent]
 })
