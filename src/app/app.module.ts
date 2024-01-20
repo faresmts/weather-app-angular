0cd78d86edcb11e2896bd0b3e77931bb24b5f564
fares <fares@stargrid.pro>
2024-01-19 23:36:06 -0300

    feat: weather card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import { WeatherHomeComponent } from './modules/wheater/page/weather-home/weather-home.component';
+import { WeatherCardComponent } from './modules/wheater/components/weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherHomeComponent
+    WeatherHomeComponent,
+    WeatherCardComponent
   ],
   // We import modules to application here
   imports: [
--- src/app/modules/wheater/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -1,15 +1,21 @@
 import { WeatherData } from 'src/app/models/interfaces/WeatherData';
 import { WeatherService } from './../../services/weather.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent implements OnInit {
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+
   initialCityName = 'Maringá';
   weatherData!: WeatherData;
+
+  searchIcon = faMagnifyingGlass;
 
   constructor (
     private weatherService: WeatherService
@@ -22,12 +28,24 @@ export class WeatherHomeComponent implements OnInit {
   getWeatherData(cityName: string): void {
     this.weatherService
     .getWeatherData(cityName) // Service method
+    .pipe(takeUntil(this.destroy$)) //sign to call the unscribe
     .subscribe({ // Receive the call result
       next: (response) => {
         response && (this.weatherData = response);
-        console.log(this.weatherData.main);
       },
       error: (error) => console.error(error)
     });
+  }
+
+  onSubmit(): void {
+    this.getWeatherData(this.initialCityName);
+    this.initialCityName = '';
+  }
+
+  // When the component is not loading, its a good pratice to unscribe
+  //from de injection to avoid memory leak
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
